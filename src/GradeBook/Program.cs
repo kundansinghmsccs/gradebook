1b0e91e57a22f451c2bf84ddcfeb0a88030eb2ec
Kundan Kumar <kundansinghmsccs@gmail.com>
2022-04-17 17:22:30 +0530

    Initial Commit

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace GradeBook 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var numbers = new[]{11,12,13,14,15};
+            var list = new List<int>{11,12,13,14,15};
+            var b=0;
+            foreach(var num in list)
+            {
+                 b = b + num ; 
+            }
+            Console.WriteLine(b);
+            int n = add(5,6);
+            Console.WriteLine(n);
+            Console.WriteLine(IsOdd(5));
+           //  Console.WriteLine($"Hello, {args[0]} , {args[1]}!");
+        }
+
+        public static int add(int a, int b)
+        {
+            return a+b ;
+        }
+        public static bool IsOdd(int val)
+        {
+            return val % 2 == 1 ;
+        }
+    }
+
+}
+
